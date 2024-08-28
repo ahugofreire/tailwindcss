5ac75c700129a3eb091f3cb5c940b1a7ae8c9237
Hugo Freire <ahugo.freire@gmail.com>
2024-08-27 23:02:51 -0300

    feat: add nav bar

---
+++ .sample.tsx
@@ -0,0 +1,20 @@
+
+// export default function Home() {
+//     return (
+//       <div className="p-8 bg-slate-50 text-slate-900 dark:bg-slate-900 dark: dark:text-slate-100 h-screen flex flex-col items-center">
+//         <div className="max-w-2xl">
+//         {/* <h1 className="font-bold text-3xl sm:text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex"> */}
+//         <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl">
+//           Using Tailwind CSS to style your site in dark mode.
+//         </h1>
+//         <p className="mt-4 dark:text-slate-400">Now that dark mode is a first-class feature of many operating systems, it’s becoming more and</p>
+  
+//         <button className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+//           Sign in
+//         </button>
+//         </div>
+//       </div>
+//     );
+//   }
+
+
